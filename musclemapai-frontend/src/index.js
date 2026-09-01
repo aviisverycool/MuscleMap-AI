6bf94d2bf1a7c69db81fbcbbe08d28a80c9001ef
Avi Tyagi <aviatray0708@gmail.com>
2026-09-01 16:40:39 -0500

    Display v1.1.0 across the site

--- musclemapai-frontend/src/index.js
+++ musclemapai-frontend/src/index.js
@@ -3,14 +3,20 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import NotFound from "./NotFound";
+import packageInfo from "../package.json";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <div className="app-version" aria-label={`MuscleMap AI version ${packageInfo.version}`}>
+        v{packageInfo.version}
+      </div>
+    </>
   </BrowserRouter>
 );
